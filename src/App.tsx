7df8ce8e0b87504b4850b145153b90358b2cf1d8
amelv <amelv@protonmail.com>
2023-10-08 17:44:44 -0400

    Add site meta tags

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import "./App.css";
 
+import { Helmet } from "react-helmet-async";
+
+const siteMetadata = {
+  title: "The Birthday Atlas",
+  description:
+    "Explore people's birthdays. See who has already celebrated, has one upcoming, or whose special day is today! ",
+};
+
 function App() {
   return (
     <>
+      <Helmet>
+        <title>{siteMetadata.title}</title>
+        <meta name="description" lang="en" content={siteMetadata.description} />
+      </Helmet>
       <h1>The Birthday Atlas</h1>
     </>
   );
