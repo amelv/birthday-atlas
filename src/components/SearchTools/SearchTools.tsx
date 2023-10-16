85f21ddca2a9ebb91f12660ed8d28f43fef22f3c
amelv <amelv@protonmail.com>
2023-10-16 07:47:10 -0400

    Prettier Changes

--- src/components/SearchTools/SearchTools.tsx
+++ src/components/SearchTools/SearchTools.tsx
@@ -1,17 +1,20 @@
-import useMediaQuery from '@/hooks/useMediaQuery'
+import useMediaQuery from "@/hooks/useMediaQuery";
 
-import { useAppContext } from '@/context/AppContext'
-import './SearchTools.css'
+import { useAppContext } from "@/context/AppContext";
+import "./SearchTools.css";
 
-function SettingsMenu () {
-  const isMobile = useMediaQuery('(max-width: 768px)')
-  const [{ isSearchToolsOpen }] = useAppContext()
+function SettingsMenu() {
+  const isMobile = useMediaQuery("(max-width: 768px)");
+  const [{ isSearchToolsOpen }] = useAppContext();
 
   return (
-    <aside aria-hidden={isMobile && !isSearchToolsOpen}
-      className={`search-tools${isMobile && isSearchToolsOpen ? ' open' : ''}`}
-    >Test</aside>
-  )
+    <aside
+      aria-hidden={isMobile && !isSearchToolsOpen}
+      className={`search-tools${isMobile && isSearchToolsOpen ? " open" : ""}`}
+    >
+      Test
+    </aside>
+  );
 }
 
-export default SettingsMenu
+export default SettingsMenu;
