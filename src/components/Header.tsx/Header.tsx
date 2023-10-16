4bc7d89ed79ff26bd1607081a341cebe75c73efa
amelv <amelv@protonmail.com>
2023-10-16 07:48:05 -0400

    Header style adjustments

--- src/components/Header.tsx/Header.tsx
+++ src/components/Header.tsx/Header.tsx
@@ -1,34 +1,32 @@
-import CakeIcon from '@/assets/cake.svg'
-import CompassIcon from '@/assets/compass.svg'
-import SettingsIcon from '@/assets/settings.svg'
-import './Header.css'
-import useMediaQuery from '@/hooks/useMediaQuery'
-import { useAppContext } from '@/context/AppContext'
+import SettingsIcon from "@/assets/settings.svg";
+import { useAppContext } from "@/context/AppContext";
+import useMediaQuery from "@/hooks/useMediaQuery";
+import "./Header.css";
 
-function Header () {
-  const isDesktop = useMediaQuery('(min-width: 768px)')
-  const [, dispatch] = useAppContext()
+function Header() {
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+  const [, dispatch] = useAppContext();
   return (
-    <header className='header-bar'>
+    <header className="header-bar">
       {isDesktop ? (
-        <span style={{ marginRight: 'auto' }} />
+        <span style={{ marginRight: "auto" }} />
       ) : (
         <button
-          className='header-bar__settings-button'
-          onClick={() => dispatch({ type: 'TOGGLE_SEARCH_TOOLS' })}
+          className="header-bar__settings-button"
+          onClick={() => dispatch({ type: "TOGGLE_SEARCH_TOOLS" })}
         >
           <img
             src={SettingsIcon}
-            className='header-bar__settings-icon'
-            alt=''
+            className="header-bar__settings-icon"
+            alt=""
           />
         </button>
       )}
-      <h1 className='header-bar__title'>The Birthday Atlas</h1>
+      <h1 className="header-bar__title">The Birthday Atlas</h1>
 
-      <span style={{ marginLeft: 'auto' }}></span>
+      <span style={{ marginLeft: "auto" }}></span>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
